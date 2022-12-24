67767dcbedb8bf7a72badc272c02fc65003f7ab1
James O'Brien <james@datavant.com>
2022-12-23 19:02:30 -0800

    Q4

--- 2022/Q4/script.js
+++ 2022/Q4/script.js
@@ -1,5 +1,13 @@
 import { readFile } from 'node:fs/promises';
 
+const rangeStringToStartEnd = (rangeString) => {
+  const [from, to] = rangeString.split('-');
+  return {
+    start: parseInt(from, 10),
+    end: parseInt(to, 10),
+  }
+}
+
 const run = async () => {
   const contents = await readFile('./2022/Q4/input.txt', { encoding: 'utf8' });
   const lines = contents.split('\n')
@@ -8,10 +16,50 @@ const run = async () => {
     if (line.trim() === '') {
       // do nothing.
     } else {
-      // todo.
+      const [sectionRangeA, sectionRangeB] = line.trim().split(',')
+      const startEndA = rangeStringToStartEnd(sectionRangeA);
+      const startEndB = rangeStringToStartEnd(sectionRangeB);
+
+      const asIsContainedInsideB = startEndA.start >= startEndB.start
+        && startEndA.end <= startEndB.end;
+      const bsIsContainedInsideA = startEndB.start >= startEndA.start
+        && startEndB.end <= startEndA.end;
+      if (asIsContainedInsideB || bsIsContainedInsideA) {
+        totalScore += 1;
+      }
     }
   });
   console.log(totalScore);
 }
 
-run();
+const part2 = async () => {
+  const contents = await readFile('./2022/Q4/input.txt', { encoding: 'utf8' });
+  const lines = contents.split('\n')
+  let totalScore = 0;
+  lines.forEach((line) => {
+    if (line.trim() === '') {
+      // do nothing.
+    } else {
+      const [sectionRangeA, sectionRangeB] = line.trim().split(',')
+      const startEndA = rangeStringToStartEnd(sectionRangeA);
+      const startEndB = rangeStringToStartEnd(sectionRangeB);
+
+      const endOfA = startEndA.end;
+      const endOfB = startEndB.end;
+
+      const startOfA = startEndA.start;
+      const startOfB = startEndB.start;
+
+      const aEndsInsideB = endOfA >= startOfB && endOfA <= endOfB
+      const aStartsInsideB = startOfA >= startOfB && startOfA <= endOfB
+      const bIsFullyInA = startOfA <= startOfB && endOfB <= endOfA
+
+      if (aEndsInsideB || aStartsInsideB || bIsFullyInA) {
+        totalScore += 1;
+      }
+    }
+  });
+  console.log(totalScore);
+}
+
+part2();
