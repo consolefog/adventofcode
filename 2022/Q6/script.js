5a58a80b455a5e1eb830dc2e5a9c60519b2b0880
James O'Brien <james@datavant.com>
2022-12-23 18:09:02 -0800

    Add empty scripts for all days

---
+++ 2022/Q6/script.js
@@ -0,0 +1,17 @@
+import { readFile } from 'node:fs/promises';
+
+const run = async () => {
+  const contents = await readFile('./2022/Q6/input.txt', { encoding: 'utf8' });
+  const lines = contents.split('\n')
+  let totalScore = 0;
+  lines.forEach((line) => {
+    if (line.trim() === '') {
+      // do nothing.
+    } else {
+      // todo.
+    }
+  });
+  console.log(totalScore);
+}
+
+run();
