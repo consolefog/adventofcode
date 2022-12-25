61d30e09365e5768e0bb4bf45f3a3db9bfad66da
James O'Brien <james@datavant.com>
2022-12-24 18:12:52 -0800

    Q12

--- 2022/Q12/script.js
+++ 2022/Q12/script.js
@@ -84,14 +84,50 @@ const canMove = (fromLetter, toLetter) => {
   return toIndex - fromIndex <= 1;
 }
 
-const run = async () => {
-  const contents = await readFile(IS_DEMO
-    ? './2022/Q12/input-demo.txt'
-    : './2022/Q12/input.txt', {encoding: 'utf8'});
+const getNewVerticesInstance = () => {
+  const contents = 'abccccccaaccaaccccaaaaacccccaaaaccccccccccccccccccccccccccccccccaaaaaaaaaaaaaaaaaaaccccccccccccccccaaaccccccccccccaacccccccccccccccccccccccccccccccccccccccccaaaa\n' +
+    'abaaaaccaaaaaccccaaaaaccccccaaaaccccccccccccccccccccaaacccccccccccaaaaaaaaaaaaaaaaaaccccccccccccccccaaaaccccccaaacaaccccccccccccccccccccccccccccccccccccccccaaaaa\n' +
+    'abaaacccaaaaaaaacaaaaaacccccaaaaccccccccccccccccccccaaaaacccccccccaaaaaaaaaaaaaaaaacccccccccaaccccaaaaaacccccccaaaaaccccaaccccccccccccccacccccccccccccccccccaaaaa\n' +
+    'abaaacccccaaaaaccccaaaaccccccaaacccccccccccccccccccaaaaaccccccccccaaaaaacacaaaaaacccccccccccaaccccaaaaacccccccccaaaaaaccaaaaaaccccccccccaaaccccacccccccccccaaaaaa\n' +
+    'abaacccccaaaaaccccaacccccccccccccccaaaaacccccccccccaaaaacccccccccaaaaaaaaccaaaaaaacccccccaaaaaaaaccaaaaacccccccaaaaaaaccaaaaacccccccccccaaacccaaaccccccccccccccaa\n' +
+    'abaaacccaaacaacccccccccccccccccccccaaaaaccccccccccccaaaaacccccccaaaaaaaaaccaaccaaacccccccaaaaaaaaccaacccccccccaaaaaaccaaaaaaccccccccccccaaaacaaaaccccccccccccccaa\n' +
+    'abaaacccccccaaccccccccccccccccccccaaaaaaccccccccccccaaccccccaacccaaaccaaaaccccccaacccccccccaaaacccccccccccccccaacaaaccaaaaaaaccccccccccccajjjjjjjcccccccccccccccc\n' +
+    'abcaacccccccccccccccccccccccccccccaaaaaaccccccccccccccccccccaaaaccccccaaaaccccccccccccccaacaaaaaccccccccccccccccccaaccccaaaaaacccccccccccjjjjjjjjjcccccaaaccccccc\n' +
+    'abccccccccccccccccccccccccccccccccaaaaaaccaaccccccccccccccaaaaaacccccccaaacccccccccccaacaaaaaaaaccccccccccccccccccccccccaaccaaccccccccaiijjjjojjjjcccccaaacaccccc\n' +
+    'abcccccccccccccccccccccccaaacccccccaaacacaaacccccccccccccccaaaaccccaaccccccccccccccccaaaaaaacccaccccccccccccccccccccccccaacccccccccccaiiijjooooojjkccaaaaaaaacccc\n' +
+    'abccccccccccccccccccccccaaaaccccccccccaaaaaccccccccccccccccaaaaacccaaaaaccccccccccccccaaaaaacccccccccccccccccccccccccccccccccccccciiiiiiiioooooookkkcaaaaaaaacccc\n' +
+    'abccccccccccccccccccccccaaaaccccccccccaaaaaaaacccccccccccccaacaaccaaaaacccccccaaacccaaaaaaaaccccccccccccccccccccccccccccccccccchiiiiiiiiooooouoookkkccaaaaaaccccc\n' +
+    'abcccccccccaaccccccccccccaaaccccccccccccaaaaacccccccccccccccccccccaaaaaccccccaaaacccaaaaacaacccccccccccccaacaacccccccccccccccchhhiiiinnnooouuuuoookkkccaaaaaccccc\n' +
+    'abcccccccccaaacccccccccccccccccccccccccaaaaacccccccccccccccccccccccaaaaacccccaaaaccccccaaccccccccccccccccaaaaacccccccccccccccchhhnnnnnnnnouuuuuuppkkkkaaaaaaccccc\n' +
+    'abccccccaaaaaaaacccaaccccccccccccccccccaacaaccaacaaccccccccccccccccaacccccccccaaaccccccaacccccccccccccccaaaaacccccccccccccccchhhnnnnnnnnntuuxuuupppkkkkkacccccccc\n' +
+    'abccccccaaaaaaaacacaaaacccccccccccccccccccaaccaaaaacccccccccccccccccccccccccccccccccccccccccccccccccccccaaaaaacccccccaacccccchhhnnnnttttttuxxxuuppppkkkkkcccccccc\n' +
+    'abcccccccaaaaaaccaaaaaaccccccccccaaccccccccccaaaaaccccccccccccccccccccccccaaacccccccccccccccccccccccccccccaaaaccaaccaaacccaaahhhnnntttttttuxxxxuupppppllllccccccc\n' +
+    'abcccccccaaaaaacccaaaacccccccccaaaaaaccccccccaaaaaacccccccccccccccccccccccaaacccccccccccccccccccccccccccccacccccaaaaaaacaaaaahhhppntttxxxxxxxxuuuuvpppplllccccccc\n' +
+    'abcccccccaaaaaacccaaaacccccccccaaaaaacccccaaaaaaaaaccccccccccccccccccccaaaaaaaacccccccccccccccccccccaaaccccccaacaaaaaaccaaaaahhhpppttxxxxxxxxyuuvvvvvppplllcccccc\n' +
+    'abcccccccaaccaacccaacaccaaaaccccaaaaacccccaaaaaaaaaccccccccccccccccccccaaaaaaaacccccccccccccccccccccaaacaaaaaaaccaaaaaaaaaaaaahhppptttxxxxxxyyyyyyvvvppplllcccccc\n' +
+    'SbccccccccccccccccccccccaaaacccaaaaacccccccaaaaaaaaacaaaccccccccaacccccccaaaaaccccccccaaaaacccccccccaaaaaaaaaaaaaaaaaaaaaaaaacgggpppttxxxxEzzyyyyyvvvqqqlllcccccc\n' +
+    'abccccccccccccccccccccccaaaacccaaaaacccccccaaaaaaaaccaaaccccccccaaacaaccaaaaaaccccccccaaaaacccccccaaaaaaaaaaaaaaaaaaaaaaaaaaacgggpppsssxxxyyyyyyvvvvvqqlllccccccc\n' +
+    'abcccaaaccccccccccccccccaaaccccccccccccccccaaaaaaaaaaaaaccccccccaaaaaaccaaaaaacccccccaaaaaacccaaaccaaaaaccaaaaaaaaaaaacccccccccgggppssswwyyyyyyvvvvqqqqlllccccccc\n' +
+    'abcaaaaaccccccccccccccccccccccccccccccccccaaaaaaaaaaaaacccccccaaaaaaacccaccaaacccccccaaaaaacccaaacccaaaaaaaaaaaccccaaacccaaaaacgggppsswwwyyyyyyvvqqqqqlllcccccccc\n' +
+    'abcaaaaaaccccccccccccccccccccccccccccccccccaaccaaaaaaaaaaaccccaaaaaaacccccccccccccccccaaaaacccaaacaaaacaaaaaaaaccccaaacccaaaaacggpppsswwwywwyyyvvqqqmmmlccccccccc\n' +
+    'abcaaaaaacccccccaacaaccccccccccccccccccccccccccaaaaaaaaaaaccccccaaaaacccccccccccccccccaaaccaaaaaaaaaaacccccccaacccccccccaaaaaacggpppsswwwwwwwwyvvqqqmmmcccccccccc\n' +
+    'abcaaaaaccccccccaaaaaccccccccccccccccccccccccccccaaaaaaaacccccccaacaaacccccccccccccccccccccaaaaaaaaaccccccccccccccccccccaaaaaagggoossswwwwrrwwwvvqqmmmccccccccccc\n' +
+    'abcaaaaacccccccaaaaaccccccccccccccccccccccccccccaaaaaaacccccccccaaccccccccccccccccccccccccccaaaaaaacccccccccccaaaccccccccaaaaagggooosssssrrrrwwwvqqmmmcccaacccccc\n' +
+    'abcccccccccccccaaaaaaccccccccccccccccccccaacccccccccaaaccccccccccccccccccccccccccccccccccccccaaaaaaccccccccccccaaaaccccccaaaccgggooosssssrrrrrwwrrqmmmcccaacccccc\n' +
+    'abcccccccccccccccaaaacccccccccccccccccccaaaacccccccacaaacccccccccccccccccccccccccccccccccccccaaaaaaacccccccccaaaaaacccccccccccgffoooooosoonrrrrrrrrmmmccaaaaacccc\n' +
+    'abcccccccccccccccaccccccccccccccccccccccaaaacccccccaaaaacccccccccccccccccccccccccccccccccccccaaacaaacccccccccaaaaacccccccccccccfffoooooooonnnrrrrrmmmddcaaaaacccc\n' +
+    'abccccccccccccccccccccccccccccccccccccccaaaaccccccccaaaaacccccccccccccccccccccccccaaaccccccccaacccccccccccccccaaaaaccccccccccccffffoooooonnnnnnrnnmmmdddaaaaacccc\n' +
+    'abcccccccccccccccccccccccccccccccccccccccccccccccccaaaaaacccccccccccccccccaaaaaccaaaacccccccccccccccccccccccccaacccccccccccccccfffffffffeeeennnnnnmmdddaaaacccccc\n' +
+    'abcccccccaaaccccccccaccccccccccccccccccccccccccccccaaaaccccccccccccaaaccccaaaaaccaaaaccccccccccccccccccccccccccccccccccccccccccccfffffffeeeeennnnnmddddaaaaaccccc\n' +
+    'abcccaaccaaacccccaaaacccccaacccccccccccccccccccccccccaaacccccccccccaaacccaaaaaacccaaaccccccccccccccccccccccccccccccccccccccccccccccffffeeeeeeeedddddddcccaacccccc\n' +
+    'abcccaaaaaaacccccaaaaaaccaaacccccccccccccccccccccccccccacccccccccccaaaaccaaaaaaccccccccccccccccccccccccccaacccccccccaaaccccccccccccccaaaaaaeeeeedddddcccccccccccc\n' +
+    'abcccaaaaaacccccccaaaacccaaacaaaccccaaaacccccccccaaacaaaccccccaacccaaaacaaaaaaacccccccccccccccccccccccccaaaccccccccaaaacccccccccccccccccccaaaaeeddddccccccccccccc\n' +
+    'abccccaaaaaaaacccaaaaaaaaaaaaaaaccccaaaacccccccccaaaaaaacccccaaacccaaaaaaaaaacccccccccccccccccccccccaaacaaaccccccccaaaaccccccccccccccccccccaaaccccccccccccccaaaca\n' +
+    'abcccaaaaaaaaacccaacaaaaaaaaaaacccccaaaaccccccccccaaaaaacaaacaaacaaaaaaaaaacccccccccccccccaaacccccccaaaaaaaaaaccccccaaaccccccccccccccccccccaacccccccccccccccaaaaa\n' +
+    'abcccaaaaaaaacccccccccccaaaaaacccccccaacccccccccccaaaaaaaaaaaaaaaaaaaaaaaaccccccccccccccccaaaacccccccaaaaaaaaacccccccccccccccccccccccccccccaaacccccccccccccccaaaa\n' +
+    'abccaaaaaaacccccccccccccaaaaaaacccccccccccccccccaaaaaaaaaaaaaaaaaaaaaaaaaaacccccccccccccccaaaacccccccaaaaaaaacccccccccccccccccccccccccccccccccccccccccccccccaaaaa\n';
   const lines = contents.split('\n')
   const vertices = []
-
-  const q = new Queue(100000);
 
   let filteredLines = lines.filter(line => line.trim() !== '');
   filteredLines.forEach((line, up) => {
@@ -99,7 +135,7 @@ const run = async () => {
     line.trim().split('').forEach((letter, index) => {
       const vertex = {
         explored: false,
-        letter: letter,
+        letter: letter === 'S' ? 'a' : letter ,
         up: filteredLines.length - 1 - up,
         right: index,
         edgeLeft: false,
@@ -109,13 +145,9 @@ const run = async () => {
         parent: undefined,
       };
       row.push(vertex);
-      if (vertex.letter === 'S') {
-        q.enqueue(vertex);
-      }
     });
     vertices.unshift(row);
   });
-
 
   const defineEdges = (vertices) => {
     for (let i = 0; i < vertices.length; i++) {
@@ -149,64 +181,114 @@ const run = async () => {
 
   defineEdges(vertices);
 
-  let end = undefined;
+  return vertices;
+}
 
-  while (!q.isEmpty() && end === undefined) {
-    const v = q.dequeue();
-    if (v.letter === 'E') {
-      end = v;
-    } else {
-      if (v.edgeLeft && !vertices[v.up][v.right - 1].explored) {
-        let w = vertices[v.up][v.right - 1];
-        w.explored = true;
-        w.parent = {
-          up: v.up,
-          right: v.right
-        };
-        q.enqueue(w);
-      }
+const run = () => {
+  const startSearch = getNewVerticesInstance()
+  const possibleStartVertices = new Set();
 
-      if (v.edgeRight && !vertices[v.up][v.right + 1].explored) {
-        let w = vertices[v.up][v.right + 1];
-        w.explored = true;
-        w.parent = {
-          up: v.up,
-          right: v.right
+  for (let i = 0; i < startSearch.length; i++) {
+    for (let j = 0; j < startSearch[i].length; j++) {
+      if (startSearch[i][j].letter === 'a') {
+        const possibleStartPoint = {
+          up: startSearch[i][j].up,
+          right: startSearch[i][j].right
         };
-        q.enqueue(w);
-      }
-
-      if (v.edgeUp && !vertices[v.up + 1][v.right].explored) {
-        let w = vertices[v.up + 1][v.right];
-        w.explored = true;
-        w.parent = {
-          up: v.up,
-          right: v.right
-        };
-        q.enqueue(w);
-      }
-
-      if (v.edgeDown && !vertices[v.up - 1][v.right].explored) {
-        let w = vertices[v.up - 1][v.right];
-        w.explored = true;
-        w.parent = {
-          up: v.up,
-          right: v.right
-        };
-        q.enqueue(w);
+        possibleStartVertices.add(possibleStartPoint);
       }
     }
   }
 
-  let current = end;
-  let path = [];
+  let best = 10000000
 
-  while (current.letter !== 'S') {
-    path.push(current.letter);
-    current = vertices[current.parent.up][current.parent.right];
-  }
+  possibleStartVertices.forEach(S => {
+    const vertices = getNewVerticesInstance();
 
-  console.log(path, path.length);
+    const q = new Queue(100000);
+
+    let enqueued = false;
+
+    for (let i = 0; i < vertices.length; i++) {
+      for (let j = 0; j < vertices[i].length; j++) {
+        if (vertices[i][j].up === S.up && vertices[i][j].right === S.right) {
+          vertices[i][j].letter = 'S';
+          q.enqueue(vertices[i][j]);
+          enqueued = true;
+        }
+      }
+    }
+
+    if (!enqueued) {
+      throw 'error';
+    }
+
+    let end = undefined;
+
+    while (!q.isEmpty() && end === undefined) {
+      const v = q.dequeue();
+      if (v.letter === 'E') {
+        end = v;
+      } else {
+        if (v.edgeLeft && !vertices[v.up][v.right - 1].explored) {
+          let w = vertices[v.up][v.right - 1];
+          w.explored = true;
+          w.parent = {
+            up: v.up,
+            right: v.right
+          };
+          q.enqueue(w);
+        }
+
+        if (v.edgeRight && !vertices[v.up][v.right + 1].explored) {
+          let w = vertices[v.up][v.right + 1];
+          w.explored = true;
+          w.parent = {
+            up: v.up,
+            right: v.right
+          };
+          q.enqueue(w);
+        }
+
+        if (v.edgeUp && !vertices[v.up + 1][v.right].explored) {
+          let w = vertices[v.up + 1][v.right];
+          w.explored = true;
+          w.parent = {
+            up: v.up,
+            right: v.right
+          };
+          q.enqueue(w);
+        }
+
+        if (v.edgeDown && !vertices[v.up - 1][v.right].explored) {
+          let w = vertices[v.up - 1][v.right];
+          w.explored = true;
+          w.parent = {
+            up: v.up,
+            right: v.right
+          };
+          q.enqueue(w);
+        }
+      }
+    }
+
+    if (end) {
+      let current = end;
+      let path = [];
+
+      while (current.letter !== 'S') {
+        path.push(current.letter);
+        current = vertices[current.parent.up][current.parent.right];
+      }
+
+      if (path.length < best) {
+        best = path.length;
+      }
+    }
+  })
+
+  console.log(best);
+
 }
 
 run();
