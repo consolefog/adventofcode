9b861c42936c016ac2ea662ad7e0fbea011ac01f
James O'Brien <581993+consolefog@users.noreply.github.com>
2023-12-04 17:30:03 -0800

    extract QUESTION_TO_RUN constant.

--- 2023/utils/consts.js
+++ 2023/utils/consts.js
@@ -1,1 +1,2 @@
 export const ROOT_DIR_2023 = '/Users/james/WebstormProjects/adventofcode/2023/';
+export const QUESTION_TO_RUN = 4;
--- 2023/utils/runQuestion.js
+++ 2023/utils/runQuestion.js
@@ -1,28 +1,29 @@
-import { question as q1 } from '../Q1/script.js';
-import { question as q2 } from '../Q2/script.js';
-import { question as q3 } from '../Q3/script.js';
-import { question as q4 } from '../Q4/script.js';
-import { question as q5 } from '../Q5/script.js';
-import { question as q6 } from '../Q6/script.js';
-import { question as q7 } from '../Q7/script.js';
-import { question as q8 } from '../Q8/script.js';
-import { question as q9 } from '../Q9/script.js';
-import { question as q10 } from '../Q10/script.js';
-import { question as q11 } from '../Q11/script.js';
-import { question as q12 } from '../Q12/script.js';
-import { question as q13 } from '../Q13/script.js';
-import { question as q14 } from '../Q14/script.js';
-import { question as q15 } from '../Q15/script.js';
-import { question as q16 } from '../Q16/script.js';
-import { question as q17 } from '../Q17/script.js';
-import { question as q18 } from '../Q18/script.js';
-import { question as q19 } from '../Q19/script.js';
-import { question as q20 } from '../Q20/script.js';
-import { question as q21 } from '../Q21/script.js';
-import { question as q22 } from '../Q22/script.js';
-import { question as q23 } from '../Q23/script.js';
-import { question as q24 } from '../Q24/script.js';
-import { question as q25 } from '../Q25/script.js';
+import {question as q1} from '../Q1/script.js';
+import {question as q2} from '../Q2/script.js';
+import {question as q3} from '../Q3/script.js';
+import {question as q4} from '../Q4/script.js';
+import {question as q5} from '../Q5/script.js';
+import {question as q6} from '../Q6/script.js';
+import {question as q7} from '../Q7/script.js';
+import {question as q8} from '../Q8/script.js';
+import {question as q9} from '../Q9/script.js';
+import {question as q10} from '../Q10/script.js';
+import {question as q11} from '../Q11/script.js';
+import {question as q12} from '../Q12/script.js';
+import {question as q13} from '../Q13/script.js';
+import {question as q14} from '../Q14/script.js';
+import {question as q15} from '../Q15/script.js';
+import {question as q16} from '../Q16/script.js';
+import {question as q17} from '../Q17/script.js';
+import {question as q18} from '../Q18/script.js';
+import {question as q19} from '../Q19/script.js';
+import {question as q20} from '../Q20/script.js';
+import {question as q21} from '../Q21/script.js';
+import {question as q22} from '../Q22/script.js';
+import {question as q23} from '../Q23/script.js';
+import {question as q24} from '../Q24/script.js';
+import {question as q25} from '../Q25/script.js';
+import {QUESTION_TO_RUN} from './consts.js';
 
 const questions = {
   q1,
@@ -52,6 +53,6 @@ const questions = {
   q25,
 }
 
-const question = 'q4';
+const question = `q${QUESTION_TO_RUN}`;
 console.log('running question', question);
 questions[question]();
