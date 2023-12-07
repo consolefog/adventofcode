45e0201afcd0237bc5b10f3f859fae07db1abe78
James O'Brien <581993+consolefog@users.noreply.github.com>
2023-12-06 18:46:13 -0800

    question 6

--- 2023/Q6/script.js
+++ 2023/Q6/script.js
@@ -2,10 +2,74 @@ import {readFileLines} from '../utils/readFileLines.js';
 import {ROOT_DIR_2023} from '../utils/consts.js';
 
 export const question = async () => {
-  const lines = await readFileLines(`${ROOT_DIR_2023}Q6/input.txt`);
+  const lines = await readFileLines(`${ROOT_DIR_2023}Q6/example.txt`);
   lines.forEach(line => {
     console.log(line);
   });
+
+  const recordsExamples = [{
+    time: 7,
+    distance: 9
+  },{
+    time: 15,
+    distance: 40
+  },{
+    time: 30,
+    distance: 200
+  }];
+
+  const records = [{
+    time: 40,
+    distance: 215
+  },{
+    time: 70,
+    distance: 1051
+  },{
+    time: 98,
+    distance: 2147
+  },{
+    time: 79,
+    distance: 1005
+  }];
+
+  let total = 1;
+
+  records.forEach(record => {
+    let wait = 0;
+    let winningWaits = 0;
+    while(wait <= record.time) {
+      let timeRemaining = record.time - wait;
+      let distance = wait * timeRemaining;
+      if (distance > record.distance) {
+        winningWaits++;
+      }
+      wait++;
+    }
+    console.log(winningWaits);
+    total *= winningWaits;
+  });
+
+  console.log('total', total);
+
+  // w^2 - w*40709879 + 215105121471005 < 0
+  // w(w - 40709879) < 215105121471005
+
+  let x = 215105121471005
+
+  console.log(x);
+
+  let winners = 0;
+
+  let time = 40709879;
+  for(let i = 0; i < time; i++) {
+    let timeRemaining = time - i;
+    let distance = i * timeRemaining;
+    if (distance > 215105121471005) {
+      winners++;
+    }
+  }
+
+  console.log(winners);
 
   return Promise.resolve();
 };
--- 2023/utils/consts.js
+++ 2023/utils/consts.js
@@ -1,2 +1,2 @@
 export const ROOT_DIR_2023 = '/Users/james/WebstormProjects/adventofcode/2023/';
-export const QUESTION_TO_RUN = 5;
+export const QUESTION_TO_RUN = 6;
